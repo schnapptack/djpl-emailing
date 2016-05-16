7f72d1b7188ebf153d8091a0b463e42a830dca83
Toni Michel <toni.michel@schnapptack.de>
2016-05-16 12:05:42 +0200

    introduced basic html email template

--- emailing/emails.py
+++ emailing/emails.py
@@ -1,7 +1,7 @@
 from django.template.loader import get_template
 from django.template import Context
 from django.core.mail import EmailMessage
-
+from django.conf import settings
 
 class TextEmail(EmailMessage):
     #content_subtype = 'text/plain'
@@ -15,6 +15,12 @@ class HtmlEmail(EmailMessage):
     def __init__(self, *args, **kwargs):
         template = kwargs.pop('template')
         context = kwargs.pop('context')
+
+        context['brand_bg'] = settings.EMAIL_BRAND_BG
+        context['brand_color'] = settings.EMAIL_BRAND_COLOR
+        context['link_color'] = settings.EMAIL_LINK_COLOR
+        context['footer_content'] = settings.EMAIL_FOOTER_CONTENT
+
         kwargs['body'] = get_template(template).render(Context(context))
         super(HtmlEmail, self).__init__(*args, **kwargs)
     
--- emailing/settings.py
+++ emailing/settings.py
@@ -1,5 +1,13 @@
-#explicitely introduce global email settings here, that other features
-#are able to use refinements consistently (from a django perspective this seems redundant)
+# coding=utf-8
+from __future__ import unicode_literals
+
+def refine_INSTALLED_APPS(original):
+    return ['emailing'] + list(original)
+
+
+
+#  explicitely introduce global email settings here, that other features
+# are able to use refinements consistently (from a django perspective this seems redundant)
 
 introduce_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 introduce_EMAIL_HOST = 'localhost'
@@ -10,4 +18,13 @@ introduce_EMAIL_USE_TLS = False
 introduce_DEFAULT_FROM_EMAIL = 'webmaster@localhost'
 introduce_SERVER_EMAIL = 'root@localhost'
 introduce_EMAIL_SUBJECT_PREFIX = '[Django] '
+
+# these colors are used to brand the email appearance
+introduce_EMAIL_BRAND_BG = '#444'
+introduce_EMAIL_BRAND_COLOR = '#fff'
+introduce_EMAIL_LINK_COLOR = '#417690'
+introduce_EMAIL_FOOTER_CONTENT = '''
+    <p>schnapptack GmbH | Schulze-Delitzsch-Straße 13 | 70569 Stuttgart</p>
+    <p><a href="http://schnapptack.de">www.schnapptack.de</a></p>
+'''
 
