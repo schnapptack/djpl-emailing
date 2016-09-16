2e0e149908433685c4072696d17a1a524c5464e4
root <hendrik@schnapptack.de>
2016-09-16 09:41:48 +0200

    improved email fallback mechanism

--- emailing/emails.py
+++ emailing/emails.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.template.loader import get_template
 from django.template import Context
 from django.core.mail import EmailMessage
 from django.conf import settings
 from premailer import transform
-import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib, os
+
 
 class TextEmail(EmailMessage):
     #content_subtype = 'text/plain'
@@ -30,13 +34,16 @@ class HtmlEmail(EmailMessage):
 
     def send(self, *args, **kwargs):
         try:
-            super(HtmlEmail, self).send(*args, **kwargs)
-        except smtplib.SMTPException:
+            super(HtmlEmail, self).send(*args, fail_silently=False, **kwargs)
+        except Exception:
+            if settings.FALLBACK_EMAIL != 'webmaster@localhost':
+                fallback_mail = settings.FALLBACK_EMAIL
+            else:
+                fallback_mail = settings.SERVER_EMAIL
             smtpObj = smtplib.SMTP('localhost')
             msg = MIMEMultipart('alternative')
-            msg['Subject'] = "Error sending Email from " + self.from_email + " to " + self.to + " in container " + os.environ['CONTAINER_DIR']
-            msg['From'] = settings.DEFAULT_FROM_EMAIL
-            msg['To'] = settings.SERVER_EMAIL
-            msg.attach(MIMEText(self.body, 'html'))
-            smtpObj.sendmail(settings.FALLBACK_EMAIL, settings.FALLBACK_EMAIL, msg.as_string())
-            raise
+            msg.attach(MIMEText(self.body.encode('utf-8'), 'html', 'utf-8'))
+            msg['From'] = fallback_mail
+            msg['To'] = fallback_mail
+            msg['Subject'] = u"Error sending Email from " + str(self.from_email) + " to " + str(self.to) + " in container " + os.environ['CONTAINER_DIR']
+            smtpObj.sendmail(fallback_mail, fallback_mail, msg.as_string())
